70e425a01320e40cd9d9cce4ee78bd266bf64d1a
jooyoung <sixwox88@gmail.com>
2019-04-11 22:55:03 +0900

    comments updated

--- cachematrix.R
+++ cachematrix.R
@@ -1,37 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Jooyoung Yu.
 
-## Write a short comment describing this function
+## To calculate large size matrix efficiently,
+## This R code have two functions for cache inverse matrix
 
+# load library to use inv function
 library(matlib)
 
+# This function returns matrix properties list which is used to cache inverse matrix
 makeCacheMatrix <- function(x = matrix()) {
     i<-NULL
+    
     set <- function(y){
         x <<- y
         i <<- NULL
     }
+    
     get <- function() x
+    
     setInverse <- function(iv) i <<- iv
+    
     getInverse <- function() i
+    
     list(set=set, get=get,
          setInverse = setInverse,
          getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
+## This function checks if the inverse matrix is already calculated.
+## It Calculate the inverse matrix at the first time, and then return cached result.
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
     i <- x$getInverse()
+    
     if(!is.null(i)){
         message("getting cached data")
         return(i)
     }
+    
     data <-x$get()
-    i<-inv(data)
+    
+    # calculate inverse matrix
+    i<-inv(data) 
+    
     x$setInverse(i)
+    
     i
 }
 
